535899108bfeeba23412f112ab6c7a04b99e7562
GuztaJF-DS <noobshippuden13>
2022-01-25 09:43:37 -0300

    another Conflict Fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 
 function Header(){
     return(
-<<<<<<< HEAD
         <div className="header">
-=======
-        <div className='header'>
->>>>>>> d17f5d01e0ea3a5680331d6bf06969a0cd3b8c12
         <div className="foxforextext">
           Fox Forex
         </div>
